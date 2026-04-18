ff4802590401da9969a7ef84bed73bc98ab95b79
eminivince <communicatti@gmail.com>
2026-04-18 01:53:38 +0100

    Remove duplicate email unique index in User schema

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -42,7 +42,6 @@ const userSchema = new Schema<IUser>(
   { timestamps: true }
 );
 
-userSchema.index({ email: 1 }, { unique: true });
 userSchema.index({ 'workspaces.workspaceId': 1 });
 
 export default mongoose.model<IUser>('User', userSchema);
