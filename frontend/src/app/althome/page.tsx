a949297dfcdf889d1ae055d6584db2320952b7b4
eminivince <communicatti@gmail.com>
2026-04-30 23:36:57 +0100

    feat(althome): layout noindex + page scaffold with AltNav/AltFooter

---
+++ frontend/src/app/althome/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import type { CSSProperties } from 'react'
+import AltNav from '@/components/marketing/AltNav'
+import AltFooter from '@/components/marketing/AltFooter'
+import { altTokens } from '@/components/marketing/alt-tokens'
+
+export default function AlthomePage() {
+  return (
+    <main style={altTokens as CSSProperties}>
+      <AltNav />
+      {/* sections will be added in subsequent tasks */}
+      <AltFooter />
+    </main>
+  )
+}
